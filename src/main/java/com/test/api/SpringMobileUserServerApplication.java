8472b2893a2a2de40aa89cf5668fae6041c8d3fc
Ivan Kristianto <ivan.kristianto@ifabula.com>
2023-04-28 20:22:53 +0700

    Initial Project

---
+++ src/main/java/com/test/api/SpringMobileUserServerApplication.java
@@ -0,0 +1,13 @@
+package com.test.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMobileUserServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMobileUserServerApplication.class, args);
+	}
+
+}
